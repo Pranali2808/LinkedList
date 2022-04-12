a4069fd352daa044c07f122a031aa5fab0342d82
pranali2808 <ranepranali1997@gmail.com>
2022-04-12 20:06:53 +0530

    ability to insert 40 after 30

--- Program.cs
+++ Program.cs
@@ -20,28 +20,40 @@ namespace LinkedList
             //linkedList.AddInReverseOrder(70);
             //linkedList.AddInReverseOrder(30);
             //linkedList.AddInReverseOrder(56);
+
             //linkedList.Append(56);
             //linkedList.Append(30);
             //linkedList.Append(70);
+
             //linkedList.Append(56);
             //linkedList.Append(70);
             //linkedList.Insert(1, 30);
+
             //linkedList.Add(56);
             //linkedList.Add(30);
             //linkedList.Add(70);
             //linkedList.RemoveFirstNode();
             //Console.WriteLine("After removing the first node of Linked List : ");
+
             //linkedList.Add(56);
             //linkedList.Add(30);
             //linkedList.Add(70);
             //linkedList.RemoveLastNode();
             //Console.WriteLine("After removing the last node of Linked List : ");
-            Console.WriteLine("Search the node from value");
+
+            //Console.WriteLine("Search the node from value");
+            //linkedList.Add(56);
+            //linkedList.Add(30);
+            //linkedList.Add(70);
+            //linkedList.Display();
+            //linkedList.Search(30);
+
+            Console.WriteLine("Insert After Value");
             linkedList.Add(56);
             linkedList.Add(30);
             linkedList.Add(70);
             linkedList.Display();
-            linkedList.Search(30);
+            linkedList.InsertAfter(30, 40);
 
             linkedList.Display();
             
